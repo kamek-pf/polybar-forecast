98d1d2e43faa52d2971669cf02505cb8ecf5b3dd
Kamek <b.kamek@gmail.com>
2018-09-18 12:07:51 -0400

    simpler error management

--- src/config.rs
+++ src/config.rs
@@ -18,22 +18,11 @@ pub fn get_config() -> Result<Configuration, Error> {
     dir.pop();
     dir.push("config.toml");
 
-    let mut f = File::open(dir)
-        .map_err(|_| ConfigError::MissingConfigFile)?;
-
+    let mut f = File::open(dir)?;
     let mut content = String::new();
-    f.read_to_string(&mut content)
-        .map_err(|_| ConfigError::CannotReadConfigFile)?;
+    f.read_to_string(&mut content)?;
 
-    let decoded: Configuration = toml::from_str(&content)
-        .map_err(|_| ConfigError::InvalidConfigFile)?;
+    let decoded: Configuration = toml::from_str(&content)?;
 
     Ok(decoded)
-}
-
-#[derive(Debug, Fail)]
-pub enum ConfigError {
-    #[fail(display = "Could not find config.toml")] MissingConfigFile,
-    #[fail(display = "Could not read config.toml")] CannotReadConfigFile,
-    #[fail(display = "Invalid config.toml")] InvalidConfigFile,
 }
